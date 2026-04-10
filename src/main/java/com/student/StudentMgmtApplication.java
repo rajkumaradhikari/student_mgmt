2aeb3e86ae1632a4ee27103e0aacf3c754ba4427
rajkumaradhikari <rajkumaradhikarime@gmail.com>
2026-04-10 15:27:37 +0545

    adding student class

---
+++ src/main/java/com/student/StudentMgmtApplication.java
@@ -0,0 +1,14 @@
+package com.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentMgmtApplication.class, args);
+		System.out.println("Testing spring boot app");
+	}
+
+}
