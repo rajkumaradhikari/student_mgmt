f1a04e0ad2d6743ab42b9efac5b132cc70c410e9
rajkumaradhikari <rajkumaradhikarime@gmail.com>
2026-05-07 22:27:06 +0545

    adding entity, auto generated id

--- src/main/java/com/student/model/Student.java
+++ src/main/java/com/student/model/Student.java
@@ -1,11 +1,18 @@
 package com.student.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
+@Entity
 public class Student {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @NotBlank(message = "Name is required")
